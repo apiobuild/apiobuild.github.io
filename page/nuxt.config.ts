788337c324fc93bdbafdeca2b83b2409f753330e
l1990790120 <l1990790120@gmail.com>
2024-07-01 18:29:59 -0400

    [try] add robots.txt

--- page/nuxt.config.ts
+++ page/nuxt.config.ts
@@ -35,12 +35,8 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       // crawlLinks: true
-      routes: ["/sitemap.xml"]
+      routes: ["/sitemap.xml", "/robots.txt"]
     }
-  },
-  robots: {
-    UserAgent: "*",
-    Disallow: ""
   },
   site: {
     url: "https://apiobuild.com",
